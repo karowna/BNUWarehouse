e7c7984379ff74b2e7e9a2dde79a76213c152d00
karowna <bilal.karowna@sky.uk>
2025-05-27 13:58:41 +0100

    adding

--- Person.py
+++ Person.py
@@ -1,4 +1,11 @@
-from abc import ABC # Abstract Base Class for Person. Customer and Supplier will inherit from this
+from abc import ABC # Abstract Base Class for Person, we will never create a Person directly
 
 class Person(ABC):
-    pass
+    def __init__(self, person_id, name, phone, email):
+        """ Initializes a Person object with name, phone, email, and id."""
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.person_id = person_id
+
+    
