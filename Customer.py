aba6a1e961a0bfc3292e7977c325fa13825261d9
karowna <bilal.karowna@sky.uk>
2025-05-27 15:47:51 +0100

    fleshing out the classes more

--- Customer.py
+++ Customer.py
@@ -1,2 +1,19 @@
 class Customer(Person):
-    pass
+    _id_counter = 1
+
+    def __init__(self, name, email, person_id):
+        super().__init__(person_id, name, email)
+        self.order_history = []
+
+    @property
+    def customer_id(self):
+        return self.person_id
+
+    @classmethod
+    def create(cls, name, email):
+        person_id = cls._id_counter
+        cls._id_counter += 1
+        return cls(name, email, person_id)
+
+    def view_order_history(self):
+        return self.order_history
--- Supplier.py
+++ Supplier.py
@@ -1,2 +1,15 @@
 class Supplier(Person):
-    pass
+    _id_counter = 1
+    def __init__(self, name, email, person_id):
+        super().__init__(person_id, name, email)
+        self.items_supplied = []
+
+    @property
+    def supplier_id(self):
+        return self.person_id
+
+    @classmethod
+    def create(cls, name, email):
+        person_id = cls._id_counter
+        cls._id_counter += 1
+        return cls(name, email, person_id)
